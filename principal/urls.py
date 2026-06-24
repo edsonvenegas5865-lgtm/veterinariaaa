1cd5daf438f3f88b34a0593a36b4ced076a11a33
edson123 <edson.venegas5865@utc.edu.ec>
2026-06-23 23:29:30 -0500

    Crear listado privado de mascotas

--- principal/urls.py
+++ principal/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='principal/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('panel/', views.panel, name='panel'),
+    path('mascotas/', views.mascotas, name='mascotas'),
 ]
--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Mascota
+
 
 def inicio(request):
     return render(request, 'principal/inicio.html')
@@ -21,3 +23,13 @@ def contactos(request):
 @login_required
 def panel(request):
     return render(request, 'principal/panel.html')
+
+
+@login_required
+def mascotas(request):
+    mascotas_registradas = Mascota.objects.all().order_by('nombre')
+    return render(
+        request,
+        'principal/mascotas.html',
+        {'mascotas': mascotas_registradas},
+    )
