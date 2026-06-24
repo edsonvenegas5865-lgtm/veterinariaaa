aa262c7d4987fe2042927ab0b216ba36ab5bae0a
jessicasangoquiza5778-ux <jessica.sangoquiza5778@utc.edu.ec>
2026-06-23 22:39:58 -0500

    Crear pagina de login

--- principal/urls.py
+++ principal/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
@@ -7,4 +8,5 @@ urlpatterns = [
     path('acerca/', views.acerca, name='acerca'),
     path('ubicacion/', views.ubicacion, name='ubicacion'),
     path('contactos/', views.contactos, name='contactos'),
+    path('login/', auth_views.LoginView.as_view(template_name='principal/login.html'), name='login'),
 ]
