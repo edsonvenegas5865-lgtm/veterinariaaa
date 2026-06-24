5866f21bc36473a77e4573b0613da1f35f823b6e
jessicasangoquiza5778-ux <jessica.sangoquiza5778@utc.edu.ec>
2026-06-23 22:18:13 -0500

    Crear pagina acerca de nosotros

--- principal/views.py
+++ principal/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'principal/inicio.html')
+
+
+def acerca(request):
+    return render(request, 'principal/acerca.html')
